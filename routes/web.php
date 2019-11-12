64f3e6717ded89ac783ebee347309ed4b4f44203
root <root@localhost.localdomain>
2019-11-12 19:34:36 +0800

    胡2

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 Route::prefix('admin/goods')->group(function () {
 
     Route::get('add', 'admin\Goods@add');
@@ -27,10 +15,6 @@ Route::prefix('api/type')->group(function () {
 
     Route::get('getall', 'api\Type@getTypeAll');
 
-        
-
-Route::get('/', function () {
-    return view('前台首页');
 });
 
 // 后台首页路由
@@ -38,7 +22,8 @@ Route::get('admin/index/index','admin\Index@index');
 
 //后台分类路由
 Route::group(['prefix'=>'admin/cates'],function(){
-	Route::get('index','admin\Cates@index');
-	Route::post('create','admin\Cates@create');
+    Route::get('index','admin\Cates@index');
+    Route::post('create','admin\Cates@create');
 
 });
+
